4ad3646f506ab0c4c315ddc469f07c43cdde8780
Andrew Alteri <andrewsalteri@gmail.com>
2020-04-15 18:59:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-def speak_to_grandma(phrase)
-    if phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == "WHAT DID YOU EAT TODAY?"
-      return "NO, NOT SINCE 1938!"
-    elsif phrase == "WHAT?"
-      return "NO, NOT SINCE 1938!"
-    elsif phrase == "Hi Nana, how are you?"
-      return "HUH?! SPEAK UP, SONNY!"
-    else phrase == "Hi!"
-      return "HUH?! SPEAK UP, SONNY!"
-    end
-  end
-=======
+
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
@@ -23,4 +9,3 @@ def speak_to_grandma(phrase)
 
   end
 end
->>>>>>> 812f55829f1c2d89fb448c23561066d48f0c3b3d
