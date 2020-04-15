229648566af5afed68e53b814d4929de494691a7
Andrew Alteri <andrewsalteri@gmail.com>
2020-04-15 18:57:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def speak_to_grandma(phrase)
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
@@ -11,3 +12,15 @@ def speak_to_grandma(phrase)
       return "HUH?! SPEAK UP, SONNY!"
     end
   end
+=======
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+     "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+
+  end
+end
+>>>>>>> 812f55829f1c2d89fb448c23561066d48f0c3b3d
